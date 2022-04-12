0958e5c48050b1014982f3354d14bb67c90c4e52
Nico <90967744+SLTechnicalTips1@users.noreply.github.com>
2022-04-12 08:03:18 +0530

    Maliya's BOT

---
+++ helpers.js
@@ -0,0 +1,22 @@
+/* Copyright (C) 2021 HIRUWA
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+T-REX HIRUWA
+*/
+
+function successfullMessage(msg) {
+    return "🎅❄️ MaliyaBot*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "☃️❄️ MaliyaBot*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "🎅❄️ MaliyaBot*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
